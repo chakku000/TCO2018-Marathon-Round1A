4a44622fdd972e53f33877cae478d4ac916c6130
chakku <chakkurai0724@outlook.jp>
2018-05-13 02:03:31 +0900

    新しく辺を作らず、クラスカルを行う

--- RoadsAndJunctions.cpp
+++ RoadsAndJunctions.cpp
@@ -1,29 +1,93 @@
-// C++11
+// 方針
+// 新しく接続点は作らずにcityだけでクラスカルを用いて最小全域木を求める
 #include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <vector>
 
+#define all(v) (v).begin(),(v).end()
+
 using namespace std;
+
+class UnionFind{
+    public:
+        vector<int> rank,p;
+        UnionFind(int n) : rank(n,0), p(n){
+            for(int i=0;i<n;i++) p[i]=i;
+        }
+        int root(int x){
+            if(p[x]==x) return x;
+            else return p[x] = root(p[x]);
+        }
+        void unite(int x,int y){
+            x=root(x);
+            y=root(y);
+            if(x==y) return;
+            if(rank[x]<rank[y]) p[x]=y;
+            else{
+                p[y]=x;
+                if(rank[x]==rank[y]) rank[x]++;
+            }
+        }
+        inline bool same(int x,int y){
+            return root(x)==root(y);
+        }
+};
+
+class City{
+    public:
+        int x,y;
+        City(){}
+        City(int x,int y) : x(x),y(y){}
+};
+
+class Edge{
+    public:
+        int a,b; 
+        double cost;
+        Edge(){}
+        Edge(int a,int b,double cost) : a(a),b(b),cost(cost){}
+        bool operator<(const Edge& e) const{ return cost<e.cost; }
+};
+
+inline double dist(const City& c1,const City& c2){
+    double dx=c1.x-c2.x;
+    double dy=c1.y-c2.y;
+    return sqrt(dx*dx+dy*dy);
+}
 
 class RoadsAndJunctions {
 public:
     int NC;
+    int NJ;
+    vector<City> cities;
 
-    vector<int> buildJunctions(int S, vector<int> cities, double junctionCost, double failureProbability) {
-        NC = cities.size() / 2;
-        // build one junction at (0,0)
+    vector<int> buildJunctions(int S, vector<int> cities_, double junctionCost, double failureProbability) {
+        NC = cities_.size() / 2;
+        for(int i=0;i<NC;i++) cities.emplace_back(City(cities_[2*i],cities_[2*i+1]));
+
         return vector<int>({});
     }
 
     // 返り値 : 建築したいroadのリスト.要素数は
     vector<int> buildRoads(vector <int> junctionStatus) {
-        // build a road from the single junction to each city
-        // (we assume that it was built, but don't check it)
-        vector<int> ret(2 * NC);
-        for (int i = 0; i < NC; ++i) {
-            ret[2 * i] = NC;
-            ret[2 * i + 1] = i;
+        NJ = junctionStatus.size();
+        vector<Edge> es((NC*NC-NC)/2);
+        int idx=0;
+        for(int i=0;i<NC;i++){
+            for(int j=i+1;j<NC;j++){
+                es[idx++]=Edge(i,j,dist(cities[i],cities[j]));
+            }
+        }
+        sort(all(es));
+
+        vector<int> ret;
+        UnionFind uf(NC);
+        for(int i=0;i<es.size();i++){
+            if(uf.same(es[i].a,es[i].b)) continue;
+            uf.unite(es[i].a,es[i].b);
+            ret.emplace_back(es[i].a);
+            ret.emplace_back(es[i].b);
         }
         return ret;
     }
